a5a447778ffd1460ff5c4b9428affb8febfe0029
Jinwoo Kim <a01047463862@gmail.com>
2024-01-11 01:38:13 +0900

    2024.01.10

---
+++ JavaScript/Section 15/if.js
@@ -0,0 +1,15 @@
+// console.log("Before Conditional");
+// let rand = Math.random();
+// if (rand < 0.5) {
+//     console.log("Your number is less than 0.5");
+//     console.log(rand);
+// }
+// console.log("After Conditional");
+
+const dayOfWeek = "Monday";
+if (dayOfWeek === "Monday") {
+    console.log("I hate Mondays");
+}
+else if (dayOfWeek === "Saturday") {
+    console.log("I love Saturday");
+}
